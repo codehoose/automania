3dab213a086d596cec6897644ea7e09daa016ede
Sloan Kelly <sloan@codehoose.com>
2025-06-08 18:11:48 +0100

    Tiled warehouse map

---
+++ tiled/8x8blocks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="8x8blocks" tilewidth="8" tileheight="8" tilecount="48" columns="4">
+ <image source="../Automania/Assets/Art/8x8blocks.png" width="32" height="96"/>
+</tileset>
---
+++ tiled/zxspectrum-palette.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="zxspectrum-palette" tilewidth="8" tileheight="8" tilecount="8" columns="8">
+ <image source="zxspectrum-palette.png" width="64" height="8"/>
+</tileset>
